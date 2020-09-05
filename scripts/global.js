cc10519ccd8c5be2f11d861d25acb608a55bca6e
Rami Abou Zahra <ramiabouzahra@gmail.com>
2020-09-05 11:41:47 +0200

    Toggle hamburger menu

--- scripts/global.js
+++ scripts/global.js
@@ -4,17 +4,13 @@ var headerContainer = document.getElementById("headerContainer");
 var bodyContainer = document.getElementById("bodyContainer");
 
 var hamburgerMenu = document.getElementById("hamburgerMenu");
-var hamburgerBtn = document.getElementsByClassName("hamburgerMenu")[0];
+var hamburgerOpenBtn = document.getElementsByClassName("hamburgerMenu")[0];
+var hamburgerCloseBtn = document.getElementsByClassName("closeBtn")[0];
 
-hamburgerBtn.addEventListener("click", () => {
-    switch (hamburgerMenu.getAttribute("class")) {
-        case null:
-            hamburgerMenu.setAttribute("class", "open");
-            document.body.setAttribute("style", "margin:unset;background:rgba(78,78,78,0.7)");
-            break;
-        case "open":
-            hamburgerMenu.removeAttribute("class");
-            document.body.setAttribute("style", "margin:unset;");
-            break;
-    }
+hamburgerOpenBtn.addEventListener("click", () => {
+    hamburgerMenu.setAttribute("class", "open");
+});
+
+hamburgerCloseBtn.addEventListener("click", () => {
+    hamburgerMenu.removeAttribute("class");
 });
